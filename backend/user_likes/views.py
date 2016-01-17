2d88b59919e229af244949e611e4e33c09df4f76
Praveen <prrraveen@gmail.com>
2016-01-17 11:47:48 +0530

    Added user like average in backend
    TODO next:
    
    Add user like graph frontend

--- backend/user_likes/views.py
+++ backend/user_likes/views.py
@@ -5,6 +5,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
 from django.utils import timezone
 from .models import Likes
+from django.db.models import Count
 
 def increment(request,username):
     try:
@@ -24,41 +25,45 @@ def like_graph(request,username,type):
         for hour in xrange(3,0,-1):
             start_datetime = timezone.now() - timedelta(hours=hour)
             end_datetime = timezone.now() - timedelta(hours=hour-1)
-            likes = Likes.objects.filter(Q(user__username = username),
-                                         Q(time__gte = start_datetime) &
-                                         Q(time__lte = end_datetime)
-                                        ).count()
-            data.append([end_datetime.hour, likes])
+            user_likes , avg_user_likes = get_counts(username,start_datetime,end_datetime)
+            data.append([end_datetime.hour, user_likes , avg_user_likes])
+
     elif type == 'day':
         for day in xrange(3,0,-1):
             start_datetime = timezone.now() - timedelta(days=day)
             end_datetime = timezone.now() - timedelta(days=day-1)
-            likes = Likes.objects.filter(Q(user__username = username),
-                                         Q(time__gte = start_datetime) &
-                                         Q(time__lte = end_datetime)
-                                        ).count()
-            data.append([end_datetime.day, likes])
+            import pdb; pdb.set_trace()
+            user_likes , avg_user_likes = get_counts(username,start_datetime,end_datetime)
+            data.append([end_datetime.day, user_likes , avg_user_likes])
+
     elif type == 'week':
         start_datetime = datetime.now() - timedelta(weeks=3)
         for week in xrange(3,0,-1):
             start_datetime = timezone.now() - timedelta(weeks=week)
             end_datetime = timezone.now() - timedelta(weeks=week-1)
-            likes = Likes.objects.filter(Q(user__username = username),
-                                         Q(time__gte = start_datetime) &
-                                         Q(time__lte = end_datetime)
-                                        ).count()
-            data.append([end_datetime.day, likes])
+            user_likes , avg_user_likes = get_counts(username,start_datetime,end_datetime)
+            data.append([end_datetime.day, user_likes , avg_user_likes])
 
     elif type == 'month':
         for month in xrange(3,0,-1):
             start_datetime = timezone.now() - timedelta(month*365/12)
             end_datetime = timezone.now() - timedelta(month*365/12-30)
-            likes = Likes.objects.filter(Q(user__username = username),
-                                         Q(time__gte = start_datetime) &
-                                         Q(time__lte = end_datetime)
-                                        ).count()
-            data.append([end_datetime.month, likes])
+            user_likes , avg_user_likes = get_counts(username,start_datetime,end_datetime)
+            data.append([end_datetime.month, user_likes , avg_user_likes])
     else:
         return HttpResponse(status=500)
 
     return HttpResponse(data)
+
+
+def get_counts(username,start_datetime,end_datetime):
+    user_likes = Likes.objects.filter(Q(user__username = username),
+                                 Q(time__gte = start_datetime) &
+                                 Q(time__lte = end_datetime)
+                                ).count()
+    all_users_likes = Likes.objects.filter(Q(time__gte = start_datetime) &
+                                           Q(time__lte = end_datetime)).count()
+
+    all_distinct_user = Likes.objects.all().values('user').annotate(dcount= Count('user')).count()
+    avg_user_likes =  all_users_likes/float(all_distinct_user)
+    return [user_likes,avg_user_likes]
